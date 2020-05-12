1d6ebdb5ce711173c7042519bd6a89d4d855e897
Brent F <feuerbacherb@gmail.com>
2020-05-12 15:34:26 -0500

    created this thing that writes HTML!

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+   if (err) throw err;
+
+   console.log('Portfolio complete! Check out index.html to see the output!')
+});
+
+
+/***
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
@@ -12,3 +28,4 @@ const printProfileData = profileDataArr => {
 };
 
 printProfileData(profileDataArgs);
+ ***/
